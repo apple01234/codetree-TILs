517eecff5485678d064167067000e6328f576e3b
apple01234 <111742198+apple01234@users.noreply.github.com>
2024-06-04 20:18:04 +0900

    Title: 점수 비교, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240604/점수 비교/score-comparison.py
@@ -0,0 +1,16 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a_math = int(arr[0])
+a_eng = int(arr[1])
+
+inp = input()
+arr = inp.split()
+b_math = int(arr[0])
+b_eng = int(arr[1])
+
+# 출력
+if a_math > b_math and a_eng > b_eng:
+	print(1)
+else:
+	print(0)

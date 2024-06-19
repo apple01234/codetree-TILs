5cbbbed1bd14a47d70126eb95dda7a3827264235
apple01234 <111742198+apple01234@users.noreply.github.com>
2024-06-19 12:26:28 +0900

    Title: 두 사람, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240619/두 사람/two-person.py
@@ -0,0 +1,10 @@
+a = input().split()
+b = input().split()
+
+if int(a[0]) >= 19 or int(b[0]) >= 19 :
+    if a[1] == 'M' or b[1] == 'M' :
+        print(1)
+    else :
+        print(0)
+else :
+    print(0)

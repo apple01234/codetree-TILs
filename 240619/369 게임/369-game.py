122b0aa7877c3fb0ef515028162a174903100d13
apple01234 <111742198+apple01234@users.noreply.github.com>
2024-06-19 11:51:57 +0900

    Title: 369 게임, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240619/369 게임/369-game.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i == 3:
+        print(0, end = " ")
+    elif i == 3:
+        print(0, end = " ")
+
+    elif i == 3:
+        print(0, end = " ")
+    else:
+        print(i, end = " ")

63dc96b4b653751df23388c696454f7634065431
최승현 <111742198+apple01234@users.noreply.github.com>
2024-09-19 11:28:41 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240919/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "Total days in Year";
+    cout << "365";
+    cout << "Circumference rate";
+    cout << "3.1415926535";
+    return 0;
+}

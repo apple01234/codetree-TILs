8824192c25726bd235515313b007d7e160fc047f
최승현 <111742198+apple01234@users.noreply.github.com>
2024-09-25 12:05:22 +0900

    Title: 두 사람, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/두 사람/two-person.py
@@ -0,0 +1,12 @@
+user1 = input()
+arr = user1.split(' ')
+a = int(arr[0])
+b = (arr[1])
+user2 = input()
+arr = user2.split(' ')
+c = int(arr[0])
+d = (arr[1])
+if 19 >= a or c and 'M' == b or d :
+    print(1)
+else :
+    print(0)

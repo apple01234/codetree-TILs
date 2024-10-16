f873528b61f3bd624b08d5f62d3872e5ab3c8f09
최승현 <111742198+apple01234@users.noreply.github.com>
2024-10-16 11:23:36 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241016/원소 10개의 합/sum-of-10-elements.c
+++ 241016/원소 10개의 합/sum-of-10-elements.c
@@ -4,7 +4,7 @@ int main() {
     int val, sum_val;
     sum_val = 0;
     for(int i = 0; i <= 10; i++) {
-        scanf("%d", val);
+        scanf("%d", &val);
         sum_val += val;
     }
     printf("%d", sum_val);

f371fff6f6514719ec099b9c28fe135b0039283b
최승현 <111742198+apple01234@users.noreply.github.com>
2024-10-16 11:32:34 +0900

    Title: 배열 채우고 출력하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241016/배열 채우고 출력하기/filling-array-and-print.c
+++ 241016/배열 채우고 출력하기/filling-array-and-print.c
@@ -6,8 +6,8 @@ int main() {
     for (int i = 0; i < 10; i++) {
         scanf("%c", &val[i]);
     }
-    for (int i = 9; i >= 0; i--) {
-        printf("%c ", val[i]);
+    for (int i = 0; i < 10; i++) {
+        printf("%s ", val[9 - i]);
     }
     return 0;
 }

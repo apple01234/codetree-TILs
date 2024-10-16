94d9694bb56c389284dfd17b0ff6c052b75e8999
최승현 <111742198+apple01234@users.noreply.github.com>
2024-10-16 11:30:14 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241016/배열 채우고 출력하기/filling-array-and-print.c
+++ 241016/배열 채우고 출력하기/filling-array-and-print.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    string val[10];
+    char val[10];
     for (int i = 0; i < 10; i++) {
     scanf("%d", &val[i]);
     }
-    for (int i = 0; i >= 0; i--) {
+    for (int i = 9; i >= 0; i--) {
         printf("%d ", val[i]);
     }
     return 0;
